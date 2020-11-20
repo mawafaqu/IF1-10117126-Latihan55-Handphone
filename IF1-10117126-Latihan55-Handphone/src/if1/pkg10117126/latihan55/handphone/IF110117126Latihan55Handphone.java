f9caf1d9affc7657f2e7278817ca00464fb78065
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-11-20 17:27:44 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan55-Handphone/src/if1/pkg10117126/latihan55/handphone/IF110117126Latihan55Handphone.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan55.handphone;
+
+/**
+ *Nama  : Mawa Faqu Rochman
+ *Kelas : IF-1
+ *NIM   : 10117126
+ *Deskripsi Program : Program ini berisi program yang menampilkan manufaktur
+ *                    dari sebuah handphone
+ */
+public class IF110117126Latihan55Handphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Handphone hp = new Handphone(" ", " ", " ", 0);
+        hp.displayProduct();
+    }
+
+}
---
+++ IF1-10117126-Latihan55-Handphone/src/if1/pkg10117126/latihan55/handphone/WindowsPhone.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan55.handphone;
+
+/**
+ *
+ * @author MAWA
+ */
+public class WindowsPhone extends Handphone{
+    private String wpKeyStore;
+
+    public String getWpKeyStore() {
+        return wpKeyStore;
+    }
+
+    public void setWpKeyStore(String wpKeyStore) {
+        this.wpKeyStore = wpKeyStore;
+    }
+
+    public WindowsPhone(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+       
+    }
+    
+}
+
